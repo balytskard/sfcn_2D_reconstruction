4f40aa4c635b688bce904741fcf7ceeb747b0405
Oleksandra <balitska.sasha@gmail.com>
2025-07-25 17:05:24 -0600

    fix datagenerator

--- classificator.py
+++ classificator.py
@@ -1,16 +1,7 @@
-import os
-import nibabel as nib
 import torch
 import torch.nn as nn
-import torch.optim as optim
-from torch.utils.data import DataLoader
 import random
 import numpy as np
-import pandas as pd
-from datagenerator_pd import DataGenerator
-import argparse
-from datetime import datetime
-from torch.utils.data import Dataset
 
 
 # Change according to the data
@@ -33,26 +24,26 @@ class SFCN(nn.Module):
         self.norm1 = nn.BatchNorm2d(32)
         self.pool1 = nn.MaxPool2d(2, stride=2, padding=0)
         
-        # self.conv2 = nn.Conv2d(32, 64, kernel_size=3, padding=1)
-        # self.norm2 = nn.BatchNorm2d(64)
-        # self.pool2 = nn.MaxPool2d(2, stride=2, padding=0)
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, padding=1)
+        self.norm2 = nn.BatchNorm2d(64)
+        self.pool2 = nn.MaxPool2d(2, stride=2, padding=0)
         
-        # self.conv3 = nn.Conv2d(64, 128, kernel_size=3, padding=1)
-        # self.norm3 = nn.BatchNorm2d(128)
-        # self.pool3 = nn.MaxPool2d(2, stride=2, padding=0)
+        self.conv3 = nn.Conv2d(64, 128, kernel_size=3, padding=1)
+        self.norm3 = nn.BatchNorm2d(128)
+        self.pool3 = nn.MaxPool2d(2, stride=2, padding=0)
         
-        # self.conv4 = nn.Conv2d(128, 256, kernel_size=3, padding=1)
-        # self.norm4 = nn.BatchNorm2d(256)
-        # self.pool4 = nn.MaxPool2d(2, stride=2, padding=0)
+        self.conv4 = nn.Conv2d(128, 256, kernel_size=3, padding=1)
+        self.norm4 = nn.BatchNorm2d(256)
+        self.pool4 = nn.MaxPool2d(2, stride=2, padding=0)
         
-        # self.conv5 = nn.Conv2d(256, 256, kernel_size=3, padding=1)
-        # self.norm5 = nn.BatchNorm2d(256)
-        # self.pool5 = nn.MaxPool2d(2, stride=2, padding=0)
+        self.conv5 = nn.Conv2d(256, 256, kernel_size=3, padding=1)
+        self.norm5 = nn.BatchNorm2d(256)
+        self.pool5 = nn.MaxPool2d(2, stride=2, padding=0)
         
-        # self.conv6 = nn.Conv2d(256, 64, kernel_size=1, padding=0)
-        # self.norm6 = nn.BatchNorm2d(64)
+        self.conv6 = nn.Conv2d(256, 64, kernel_size=1, padding=0)
+        self.norm6 = nn.BatchNorm2d(64)
         
-        # self.avgpool = nn.AvgPool2d(2)
+        self.avgpool = nn.AvgPool2d(2)
         self.dropout = nn.Dropout(0.2)
         
         self.flattened_size = self._get_flattened_size()
@@ -62,23 +53,23 @@ class SFCN(nn.Module):
     def _get_flattened_size(self):
         x = torch.zeros(1, 1, params['imagex'], params['imagey'])
         x = self.pool1(self.norm1(self.conv1(x)))
-        # x = self.pool2(self.norm2(self.conv2(x)))
-        # x = self.pool3(self.norm3(self.conv3(x)))
-        # x = self.pool4(self.norm4(self.conv4(x)))
-        # x = self.pool5(self.norm5(self.conv5(x)))
-        # x = self.norm6(self.conv6(x))
-        # x = self.avgpool(x)
+        x = self.pool2(self.norm2(self.conv2(x)))
+        x = self.pool3(self.norm3(self.conv3(x)))
+        x = self.pool4(self.norm4(self.conv4(x)))
+        x = self.pool5(self.norm5(self.conv5(x)))
+        x = self.norm6(self.conv6(x))
+        x = self.avgpool(x)
         x = self.dropout(x)
         return x.view(1, -1).shape[1]
     
     def forward(self, x):
         x = self.pool1(torch.relu(self.norm1(self.conv1(x))))
-        # x = self.pool2(torch.relu(self.norm2(self.conv2(x))))
-        # x = self.pool3(torch.relu(self.norm3(self.conv3(x))))
-        # x = self.pool4(torch.relu(self.norm4(self.conv4(x))))
-        # x = self.pool5(torch.relu(self.norm5(self.conv5(x))))
-        # x = torch.relu(self.norm6(self.conv6(x)))
-        # x = self.avgpool(x)
+        x = self.pool2(torch.relu(self.norm2(self.conv2(x))))
+        x = self.pool3(torch.relu(self.norm3(self.conv3(x))))
+        x = self.pool4(torch.relu(self.norm4(self.conv4(x))))
+        x = self.pool5(torch.relu(self.norm5(self.conv5(x))))
+        x = torch.relu(self.norm6(self.conv6(x)))
+        x = self.avgpool(x)
         x = self.dropout(x)
         x = torch.flatten(x, 1)
         x = torch.relu(self.fc1(x))
